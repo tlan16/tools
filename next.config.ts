8288b08343e2c2297f4f7a85f82bd80bf7dff56e
Frank Lan <franklan118@gmail.com>
2025-08-30 13:49:46 +1000

    Revert "add webpack-bundle-analyzer support and optimize package imports"
    This reverts commit a8516d419bc92e9986b88f02ac3fa01fd8c1d399.

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Enable bundle analyzer when ANALYZE=true
-  ...(process.env.ANALYZE === 'true' && {
-    webpack: (config: any) => {
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          openAnalyzer: false,
-        })
-      );
-      return config;
-    },
-  }),
-
-  // Optimize bundle splitting
-  experimental: {
-    optimizePackageImports: ['lucide-react', '@radix-ui/react-dialog'],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
